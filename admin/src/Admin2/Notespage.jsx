604ef053b9a5788d496f17182ae725bb41854e1d
Rashid Khopatkar <rashidkhopatkar@gmail.com>
2025-10-26 22:19:53 +0530

    Update Notespage.jsx

--- admin/src/Admin2/Notespage.jsx
+++ admin/src/Admin2/Notespage.jsx
@@ -17,7 +17,8 @@ const NotesPage = () => {
     "Computer",
     "Mechanical",
     "Civil",
-    "Electrical",
+    "Electrical"
+    "Information Technology",
   ];
   const semesters = ["1", "2", "3", "4", "5", "6", "7", "8"];
 
